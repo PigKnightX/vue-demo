505eb135433da46ddb42c55175b40602dca759e2
PigKnightX <PigKnightX@163.com>
2021-12-09 15:41:27 +0800

    feat: vue-draggable初步使用

---
+++ vue-demo/src/pages/DraggableDemo.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <draggable v-model="myArray" group="people" @start="drag=true" @end="drag=false">
+            <div 
+                v-for="element in myArray" 
+                :key="element.id"
+                class="draggable-item"
+            >
+                {{element.name}}
+            </div>
+        </draggable>
+    </div>
+</template>
+<script>
+import draggable from 'vuedraggable'
+
+export default {
+    components: {
+        draggable
+    },
+    data() {
+        return {
+            myArray: [
+                {id: 1, name: '111'},
+                {id: 2, name: '222'},
+                {id: 3, name: '333'}
+            ]
+        }
+    }
+    
+}
+</script>
+<style lang="css" scoped>
+.draggable-item {
+    border: 1px solid gray;
+    margin: 5px;
+    display: inline-flex
+}
+</style>
--- vue-demo/src/router/index.js
+++ vue-demo/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '@/pages/HelloWorld'
+import Draggable from '@/pages/DraggableDemo.vue'
 
 Vue.use(Router)
 
@@ -8,8 +9,17 @@ export default new Router({
   routes: [
     {
       path: '/',
+      redirect: '/draggableDemo'
+    },
+    {
+      path: '/helloWorld',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/draggableDemo',
+      name: 'draggableDemo',
+      component: Draggable
     }
   ]
 })
